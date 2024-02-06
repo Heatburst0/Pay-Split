031c9f677ab59c262cf75c136ab2f5c51dd6d8a0
krishna verma <krishna.verma63976@gmail.com>
2024-02-06 23:23:42 +0530

    CreateActivity changed and forgotpassword view updated

--- app/src/main/java/com/example/paysplit/CreateActivity.kt
+++ app/src/main/java/com/example/paysplit/CreateActivity.kt
@@ -2,10 +2,23 @@ package com.example.paysplit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.paysplit.databinding.ActivityCreateBinding
 
 class CreateActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityCreateBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create)
+        binding = ActivityCreateBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupActionBar()
+    }
+    private fun setupActionBar() {
+
+        setSupportActionBar(binding.toolbarCreate)
+        binding.toolbarCreate.setNavigationIcon(R.drawable.ic_white_color_back_24dp)
+
+        binding.toolbarCreate.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }
