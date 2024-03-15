8f7a96059d19105508c936035aca51106532576d
krishna verma <krishna.verma63976@gmail.com>
2024-03-15 19:48:59 +0530

    fixed profile picture set and profile view

--- app/src/main/java/com/example/paysplit/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/paysplit/fragments/ProfileFragment.kt
@@ -1,9 +1,18 @@
 package com.example.paysplit.fragments
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.example.paysplit.MainActivity
@@ -11,6 +20,8 @@ import com.example.paysplit.R
 import com.example.paysplit.databinding.FragmentProfileBinding
 import com.example.paysplit.firebase.FirestoreClass
 import com.example.paysplit.models.User
+import com.example.paysplit.utils.Constants
+import java.io.IOException
 
 /**
  * A simple [Fragment] subclass.
@@ -19,26 +30,44 @@ import com.example.paysplit.models.User
  */
 class ProfileFragment : Fragment() {
 
+    private var mSelectedImageFileUri: Uri? = null
     private lateinit var binding : FragmentProfileBinding
-
+    private var dataCollected = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        binding = FragmentProfileBinding.inflate(layoutInflater)
+        if(!dataCollected) {
+            (activity as MainActivity).showProgressDialog()
+            FirestoreClass().loadUserData(this)
+            dataCollected=true
+        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
-        binding = FragmentProfileBinding.inflate(inflater,container,false)
-        (activity as MainActivity).showProgressDialog()
-        FirestoreClass().loadUserData(this)
+
+        binding.ivProfileUserImage.setOnClickListener {
+            Toast.makeText(activity,"pressed",Toast.LENGTH_SHORT).show()
+            if (ContextCompat.checkSelfPermission(activity as MainActivity, Manifest.permission.READ_MEDIA_IMAGES)
+                == PackageManager.PERMISSION_GRANTED
+            ) {
+                val galleryIntent = Intent(
+                    Intent.ACTION_PICK,
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                )
+                startActivityForResult(galleryIntent,Constants.PICK_IMAGE_REQUEST_CODE)
+            } else {
+
+                ActivityCompat.requestPermissions(
+                   this.activity as MainActivity,
+                    arrayOf(Manifest.permission.READ_MEDIA_IMAGES),
+                    Constants.READ_STORAGE_PERMISSION_CODE
+                )
+            }
+        }
         return binding.root
-    }
-
-    companion object {
-
     }
     fun setUserdata(user : User){
         (activity as MainActivity).cancelDialog()
@@ -52,6 +81,54 @@ class ProfileFragment : Fragment() {
                 .centerCrop()
                 .placeholder(R.drawable.ic_user_place_holder)
                 .into(binding.ivProfileUserImage)
+        }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK
+            && requestCode == Constants.PICK_IMAGE_REQUEST_CODE
+            && data!!.data != null
+        ) {
+            // The uri of selection image from phone storage.
+            Toast.makeText(activity,"Yesss",Toast.LENGTH_SHORT).show()
+            mSelectedImageFileUri = data.data!!
+
+            try {
+                // Load the user image in the ImageView.
+                Glide
+                    .with(this)
+                    .load(Uri.parse(mSelectedImageFileUri.toString())) // URI of the image
+                    .centerCrop() // Scale type of the image.
+                    .placeholder(R.drawable.ic_user_place_holder) // A default place holder
+                    .into(binding.ivProfileUserImage) // the view in which the image will be loaded.
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == Constants.READ_STORAGE_PERMISSION_CODE) {
+            //If permission is granted
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                val galleryIntent = Intent(
+                    Intent.ACTION_PICK,
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                )
+                startActivityForResult(galleryIntent,Constants.PICK_IMAGE_REQUEST_CODE)
+            } else {
+                //Displaying another toast if permission is not granted
+                Toast.makeText(
+                    activity,
+                    "Oops, you just denied the permission for storage. You can also allow it from settings.",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
         }
     }
 }
--- app/src/main/java/com/example/paysplit/utils/Constants.kt
+++ app/src/main/java/com/example/paysplit/utils/Constants.kt
@@ -1,5 +1,24 @@
 package com.example.paysplit.utils
 
+import android.app.Activity
+import android.content.Intent
+import android.provider.MediaStore
+import androidx.core.app.ActivityCompat.startActivityForResult
+
 object Constants {
     const val Users : String  ="Users"
+    fun showImageChooser(activity: Activity) {
+        // An intent for launching the image selection of phone storage.
+        val galleryIntent = Intent(
+            Intent.ACTION_PICK,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        )
+        // Launches the image selection of phone storage using the constant code.
+
+        activity.startActivityForResult(galleryIntent, PICK_IMAGE_REQUEST_CODE)
+    }
+
+    const val READ_STORAGE_PERMISSION_CODE = 1
+    // A unique code of image selection from Phone Storage.
+    const val PICK_IMAGE_REQUEST_CODE = 2
 }
